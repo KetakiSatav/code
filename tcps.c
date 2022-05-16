7959823dfb6eb5e8e773239e151aa5a374503389
Ketaki Satav <ketaki@LAPTOP-CN3U58ER>
2022-05-16 22:52:03 +0530

    First Commit

---
+++ tcps.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+void error{
+    
+}
